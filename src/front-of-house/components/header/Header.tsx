e4966111365f5189cac2a31273347a151c4167a4
Micha Alt <micha@haus23.net>
2021-12-28 20:38:54 +0100

    Adjust dark mode.

--- src/front-of-house/components/header/Header.tsx
+++ src/front-of-house/components/header/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => (
-  <nav className="bg-white shadow-lg">
+  <nav className="bg-white shadow-lg dark:bg-gray-800 dark:shadow-none dark:border-b dark:border-gray-700">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center h-16">
         <h1 className="text-2xl font-semibold">runde.tips</h1>
