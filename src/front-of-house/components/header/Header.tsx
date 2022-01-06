86f64044eb70821fc0ba1fd0207dee6a61474fbd
Micha Alt <micha@haus23.net>
2022-01-06 12:53:42 +0100

    Turn brand into home anchor.

--- src/front-of-house/components/header/Header.tsx
+++ src/front-of-house/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import { Logo } from '@/common/components/logo/Logo';
 import { ThemeSwitch } from '@/common/components/theme-switch/ThemeSwitch';
+import { Link } from 'react-router-dom';
 
 export const Header = () => (
   <nav className="bg-white shadow-lg dark:bg-gray-800 dark:shadow-none dark:border-b dark:border-gray-700">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16">
-        <div className="shrink-0 flex items-center">
-          <Logo className="h-8" />
-          <h1 className="ml-2 text-2xl font-semibold">runde.tips</h1>
+        <div className="shrink-0 flex">
+          <Link className="flex items-center" to="/">
+            <Logo className="h-8" />
+            <h1 className="ml-2 text-2xl font-semibold">runde.tips</h1>
+          </Link>
         </div>
         <div className="flex items-center">
           <ThemeSwitch />
