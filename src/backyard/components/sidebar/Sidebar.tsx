4a7b3abb5f26c5b5f30a0d15f0935ec627d6e38e
Micha Alt <micha@haus23.net>
2022-01-31 11:58:43 +0100

    Handle visibility of sidebar links.

--- src/backyard/components/sidebar/Sidebar.tsx
+++ src/backyard/components/sidebar/Sidebar.tsx
@@ -1,56 +1,68 @@
-import { useCurrentChampionship } from '@/backyard/hooks/use-current-championship';
-import { Logo } from '@/common/components/logo/Logo';
-import { useAuth } from '@/common/hooks/use-auth';
+import {useCurrentChampionship} from '@/backyard/hooks/use-current-championship';
+import {Logo} from '@/common/components/logo/Logo';
+import {useAuth} from '@/common/hooks/use-auth';
 import {
   CalendarIcon,
   FolderIcon,
   HomeIcon,
   UserIcon,
 } from '@heroicons/react/outline';
-import { Link } from 'react-router-dom';
-import { SidebarLink } from '../sidebar-link/SidebarLink';
+import {Link} from 'react-router-dom';
+import {SidebarLink} from '../sidebar-link/SidebarLink';
+import {useRounds} from "@/backyard/hooks/use-rounds";
+import {ElementType} from "react";
+import {Championship} from "@/api/model/championship";
+import {Round} from "@/api/model/round";
+
+
+const sidebarLinks: {
+  to: string,
+  icon: ElementType,
+  label: string,
+  visible: (championship: Championship, rounds: Round[]) => boolean
+}[] = [
+  {
+    to: '.',
+    icon: HomeIcon,
+    label: 'Dashboard',
+    visible: () => true
+  },
+  {
+    to: './turnier',
+    icon: FolderIcon,
+    label: 'Turnier',
+    visible: (championship) => championship !== null
+  },
+  {
+    to: './spiele',
+    icon: CalendarIcon,
+    label: 'Spiele',
+    visible: (championship, rounds) => championship !== null && rounds?.length > 0
+  }
+];
 
 export type SidebarProps = {
   onNavAction?: () => void;
 };
 
-export const Sidebar = ({ onNavAction }: SidebarProps) => {
-  const { user } = useAuth();
-  const { championship } = useCurrentChampionship();
+export const Sidebar = ({onNavAction}: SidebarProps) => {
+  const {user} = useAuth();
+  const {championship} = useCurrentChampionship();
+  const {rounds} = useRounds();
 
   return (
     <div className="flex grow flex-col border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
       <div className="flex grow flex-col overflow-y-auto">
         <div className="flex h-14 shrink items-center px-4 md:h-16">
           <Link onClick={onNavAction} className="flex items-center" to="/">
-            <Logo className="h-8" />
+            <Logo className="h-8"/>
             <h1 className="ml-2 text-2xl font-semibold">runde.tips</h1>
           </Link>
         </div>
         <nav className="mt-2 grow space-y-1 px-2">
-          <SidebarLink onClick={onNavAction} to="." icon={HomeIcon} end>
-            Dashboard
-          </SidebarLink>
-          {championship && (
-            <>
-              <SidebarLink
-                onClick={onNavAction}
-                to="./turnier"
-                icon={FolderIcon}
-                end
-              >
-                Turnier
-              </SidebarLink>
-              <SidebarLink
-                onClick={onNavAction}
-                to="./spiele"
-                icon={CalendarIcon}
-                end
-              >
-                Spiele
-              </SidebarLink>
-            </>
-          )}
+          {sidebarLinks.filter(link => link.visible(championship, rounds)).map((link, ix) =>
+            <SidebarLink key={ix} to={link.to} icon={link.icon}>{link.label}</SidebarLink>)
+          }
         </nav>
       </div>
       <div className="shrink border-t border-gray-200 p-4 dark:border-gray-700">
@@ -64,7 +76,8 @@ export const Sidebar = ({ onNavAction }: SidebarProps) => {
                   alt="User Avatar"
                 />
               ) : (
-                <UserIcon className="inline-block h-9 w-9 rounded-full bg-gray-200 p-1 text-gray-400 dark:bg-gray-900 dark:text-gray-300" />
+                <UserIcon
+                  className="inline-block h-9 w-9 rounded-full bg-gray-200 p-1 text-gray-400 dark:bg-gray-900 dark:text-gray-300"/>
               )}
             </div>
             <div className="ml-3 grow">
